6797a88cc57096d046d375ae532b6be0f96ad17c
Dvxy67 <jean.ardaillou1@gmail.com>
2025-05-21 20:59:20 +0200

    new db() interface

--- config/db.php
+++ config/db.php
@@ -1,15 +1,23 @@
 <?php
-$host = 'localhost';
-$db   = 'seedarrt';
-$user = 'seedarrt';
-$pass = 'seedarrt';
-$charset = 'utf8mb4';
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
-$pdo = new PDO($dsn, $user, $pass, $options);
+function db(){
+    static $pdo;
+    if($pdo === null){
+        $host = 'localhost';
+        $db   = 'seedarrt';
+        $user = 'seedarrt';
+        $pass = 'seedarrt';
+        $charset = 'utf8mb4';
+        
+        $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+        $options = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+        $pdo = new PDO($dsn, $user, $pass, $options);
+    }
+    return $pdo;
+}
+
 ?>
